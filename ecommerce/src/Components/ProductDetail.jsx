86ff83fbb39e5083e916e8205cf2653d6c9a7703
amanidreesi67 <amanidreesi67@gmail.com>
2025-09-17 09:54:47 +0530

    Hello

--- ecommerce/src/Components/ProductDetail.jsx
+++ ecommerce/src/Components/ProductDetail.jsx
@@ -2,21 +2,36 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { fetchProductDetail } from '../Redux/product/action'
+import { ClipLoader } from 'react-spinners'
+
 
 function ProductDetail() {
-    const { loading, error, productDetail } = useSelector((state) => state.productState );
-    
-    const {id} = useParams()
-    const dispatch = useDispatch()
-    useEffect(()=>{
-        dispatch(fetchProductDetail(id))
-    },[dispatch,id])
+  const { loading, error, productDetail } = useSelector((state) => state.productState);
+
+  const { id } = useParams()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchProductDetail(id))
+  }, [dispatch, id])
+
+
+
+  if (loading) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <ClipLoader colort="#3B82F6" size={50} />
+      </div>
+    )
+  }
+  if (error) {
+    <p>{error}</p>
+  }
   return (
     <div>
       <h2>Product details</h2>
       {productDetail.title}
       <img src={productDetail.images} className='w-[200px]' />
-      
+
     </div>
   )
 }
